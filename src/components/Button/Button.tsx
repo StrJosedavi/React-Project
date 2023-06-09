435077e5eba7e492d2536627f5bc6ad0d92f556d
StrJosedavi <josedavi100@gmail.com>
2023-06-08 23:25:44 -0300

    Adicionando primeiro componente

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,26 @@
+
+import { ContainerButton, IconButton, TypographyButton } from './styles';
 import React from "react";
 
+
 interface ButtonProps{
-    
+    text: String
+    Icon: string
 }
 
-export const Button: React.FC<ButtonProps> = () => ();
+export const Button: React.FC<ButtonProps> = ({ text, Icon }) => {
+
+    return (
+       <ContainerButton>
+            
+                <IconButton>
+                    <img src={Icon} alt="IconButton" />
+                </IconButton>
+            
+                <TypographyButton>{text}</TypographyButton> 
+           
+       </ContainerButton>
+    );
+}
+
+export default Button;
