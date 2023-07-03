6b84d19f7e1274ddadb6642c5b2a110ad132054f
Jose Davi Fernandes Sobreira <davi.sobreira@fitbank.com.br>
2023-07-03 11:01:58 -0300

    config: Ajustando configurações do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    // vite config
-    define: {
-      __APP_ENV__: env.APP_ENV,
-    },
-    base: 'http://localhost:3000',
-    root: './src',
-  }
-})
+export default defineConfig({
+  server: {
+    open: true,
+  },
+  build: {
+    target: "esnext",
+    polyfillDynamicImport: false,
+  },
+});
